4b2c4695b7ef17e25bf356594ce3a84cd2205045
Timothy RYAN <ryaninlux@gmail.com>
2022-07-16 14:30:53 -0700

    Create an order constructor in order.cs

---
+++ OrderOrganizer/Models/Order.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+
+namespace OrderOrganizer.Models
+{
+  public class Order
+  {
+    private static List<Order> _instances = new List<Order> {};
+    private static int _PastryPrice = 3;
+  
+    public string Name { get; set; }
+    public string Title  { get; set;}
+    public int TotalPrice { get; set; }
+    public int PhoneNumber { get; set; }
+    public string Date { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
+    
+    public Order(string name,string title ,int totalprice,int phonenumber,string date)
+    {
+      Name = name;
+      Title = title;
+      TotalPrice = totalprice * _PastryPrice;
+      PhoneNumber = phonenumber;
+      Date = date;
+      _instances.Add(this);
+      Id = _instances.Count;      
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+
+  }
+}
