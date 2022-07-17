20039fca299c6ef15d9daa508891c370386808a0
Timothy RYAN <ryaninlux@gmail.com>
2022-07-17 11:16:11 -0700

    Finish with vendor controller and vendor cshtml

--- OrderOrganizer/Controllers/VendorsController.cs
+++ OrderOrganizer/Controllers/VendorsController.cs
@@ -18,6 +18,34 @@ namespace OrderOrganizer.Controllers
     {
       return View();
     }
-  }
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("order", vendorOrders);
+      return View(model);
+    }
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string name,string title,int phonenumber, int totalprice, string date)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor findVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(name, title, phonenumber, totalprice, date);
+      findVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = findVendor.Orders;
+      model.Add("order", vendorOrders);
+      model.Add("vendor", findVendor);
+      return View("Show", model);
+    }
 
+  }
 }
